21c074353589ac539a2f678c25b146643539d082
immalz <alzamora1008@gmail.com>
2020-11-02 11:39:37 -0500

    initial commi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Pipes
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+// Rutas
+import { ROUTES } from './app.routes';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactoComponent,
+    AboutComponent,
+    DomseguroPipe,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot( ROUTES, {useHash: true} ),
     AppRoutingModule
   ],
   providers: [],
